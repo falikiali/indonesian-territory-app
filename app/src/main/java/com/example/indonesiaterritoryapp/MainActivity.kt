14592834446a0d6e75cadbc4ad1eb906bceda875
Ali Faliki <alifaliki456@gmail.com>
2022-12-07 11:57:57 +0700

    add spinner for province and regency

--- app/src/main/java/com/example/indonesiaterritoryapp/MainActivity.kt
+++ app/src/main/java/com/example/indonesiaterritoryapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.indonesiaterritoryapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModel
 import com.example.indonesiaterritoryapp.databinding.ActivityMainBinding
+import com.example.indonesiaterritoryapp.domain.model.Province
+import com.example.indonesiaterritoryapp.domain.model.Regency
 import com.example.indonesiaterritoryapp.presentation.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,25 +17,80 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
+    private var listProvince = ArrayList<String>()
+    private var listRegency = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        handleProvinces()
+        observeProvince()
+        observeRegencies()
+    }
+
+    private fun handleProvinces() {
         mainViewModel.getProvinces()
-        mainViewModel.getRegencies("11.json")
+    }
 
-        mainViewModel.resultProvince.observe(this) {
-            Log.d("MAIN ACTIVITY", it.toString())
+    private fun handleRegencies(id: String) {
+        val provinceId = "$id.json"
+        mainViewModel.getRegencies(provinceId)
+    }
+
+    private fun observeProvince() {
+        mainViewModel.resultProvince.observe(this@MainActivity) {
+            addListProvince(it)
+            handleSpinnerProvince(it)
         }
+    }
 
-        mainViewModel.resultRegency.observe(this) {
-            Log.d("TAG REGENCY", it.toString())
+    private fun addListProvince(data: List<Province>) {
+        listProvince.clear()
+        listProvince.add("Pilih Provinsi")
+        data.forEach {
+            listProvince.add(it.name)
         }
+    }
 
-        mainViewModel.error.observe(this@MainActivity) {
-            Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_SHORT).show()
-            Log.e("ERROR REGENCY", it.toString())
+    private fun handleSpinnerProvince(data: List<Province>) {
+        binding.spinnerProvince.apply {
+            setItems(listProvince)
+            setOnItemSelectedListener { _, _, _, item ->
+                if (item != "Pilih Provinsi") {
+                    data.forEach {
+                        if (item == it.name) {
+                            handleRegencies(it.id)
+                        }
+                    }
+                } else {
+                    binding.spinnerRegency.visibility = View.GONE
+                    listRegency.clear()
+                }
+            }
+        }
+    }
+
+    private fun observeRegencies() {
+        mainViewModel.resultRegency.observe(this@MainActivity) {
+            addListRegency(it)
+            handleSpinnerRegency()
+        }
+    }
+
+    private fun addListRegency(data: List<Regency>) {
+        listRegency.clear()
+        listRegency.add("Pilih Kota/Kabupaten")
+        data.forEach {
+            listRegency.add(it.name)
+        }
+    }
+
+    private fun handleSpinnerRegency() {
+        binding.spinnerRegency.apply {
+            visibility = View.VISIBLE
+            setItems(listRegency)
         }
     }
 }
